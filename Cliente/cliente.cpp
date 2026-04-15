7642f8e7dba531739ff9534731e9d577c67b49d1
Rai Mattos <mattosrai4@gmail.com>
2026-04-15 15:40:43 -0300

    refatoracao do codigo do cliente

--- Cliente/cliente.cpp
+++ Cliente/cliente.cpp
@@ -18,39 +18,37 @@
 #include <arpa/inet.h>
 #include <cstring>
 
+// função para adição das chamadas rpc
 #include "rpc/server.h"
 #include "rpc/client.h"
 
+// abrevisações de funções do C++
 namespace fs = std::filesystem;
 
+//definições de constantes utilizadas no códigos
 #define download 1
 #define upload 2
 #define privado 3
 #define somente_leitura 4
 #define escrita 5
 
-// --- VARIÁVEIS GLOBAIS ---
-int minha_porta;
-std::string meu_usuario;
-std::string meu_diretorio;
-std::string meu_ip_atual;
-std::vector<std::string> meus_arquivos; // Histórico legado
-std::mutex acesso_tabela;
-
+// estrutura de dados para salvar os metadados dos arquivos relacionados ao cliente
 struct EstadoLocal {
     int versao;
     std::filesystem::file_time_type ultima_modificacao;
     int permissao;
 };
 
+//variáveis de uso global
+int minha_porta;
+std::string meu_usuario;
+std::string meu_diretorio;
+std::string meu_ip_atual;
+std::mutex acesso_tabela; // variável de semáforo de acesso a tabela de arquivos globais
 std::map<std::string, EstadoLocal> meus_arquivos_estados;
-rpc::client tracker("186.217.124.198", 8000); // Tracker Global para o Menu
+rpc::client tracker("186.217.124.198", 8000); // bind do peer tracker
 
-// ========================================================
-// 1. DESCOBERTA DE REDE (IP DINÂMICO)
-// ========================================================
-
-// Abre um socket cego para o DNS do Google só para o Linux nos contar nosso próprio IP
+// função adiquirida por IA para obtenção do IP do peer por meio de comunicação ao servidor do google
 std::string obter_meu_ip() {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0) return "127.0.0.1";
@@ -83,10 +81,7 @@ std::string obter_meu_ip() {
     return ip_descoberto;
 }
 
-// ========================================================
-// 2. LÓGICA DE DOWNLOAD
-// ========================================================
-
+//função que faz o download de acordo com os pares de ip e porta devolvida pelo servidor tracker, futuramente adicionar lógica de partição de arquivo para fazer o download de várias fontes
 void fazer_download(std::string nome_arq, rpc::client& tracker_ativo) {
     std::string caminho_completo = meu_diretorio + "/" + nome_arq;
     
@@ -120,14 +115,11 @@ void fazer_download(std::string nome_arq, rpc::client& tracker_ativo) {
     }
 }
 
-// ========================================================
-// 3. O VIGIA (THREAD DE BACKGROUND)
-// ========================================================
-
+//função de sincronização automática dos arquivos baixados e compartilhados, também realiza a verificação de ip para que o código também permita a mobilidade e transição do computador com mudanças de IP no meio do programa
 void sincronizador_automatico() {
    
-
     while (true) {
+        // a função roda sempre a cada 5 segundos
         std::this_thread::sleep_for(std::chrono::seconds(5));
 
         try {
@@ -138,46 +130,45 @@ void sincronizador_automatico() {
             }
         } catch (...) {}
 
+        //passa por todos arquivos pertencentes ao diretório do atual usuário logado
         for (const auto& entrada : fs::directory_iterator(meu_diretorio)) {
             if (entrada.is_regular_file()) {
                 std::string nome_arq = entrada.path().filename().string();
 
+                //elimina tipos de arquivos especiais no qual podem ser gerados
                 if (nome_arq.front() == '.' || nome_arq.back() == '~') {
                     continue; 
                 }
 
+                //obtem o tempo atual do arquivo através do seu metadado
                 auto tempo_atual = std::filesystem::last_write_time(entrada.path());
 
-                // ===================================================
-                // PASSO 1: LEITURA RÁPIDA (Trava e destrava imediatamente)
-                // ===================================================
-                bool eh_novo = false;
+                
+                bool arquivo_adicionado = false;
                 bool foi_modificado = false;
                 bool verificar_rede = false;
                 int versao_local = 0;
 
-                { // Abre um mini-escopo só para o Lock de Leitura
+                { // pede acesso para fazer leituras na tabela de dados e realiza todas as operações
                     std::lock_guard<std::mutex> lock_leitura(acesso_tabela);
                     
                     if (meus_arquivos_estados.find(nome_arq) == meus_arquivos_estados.end()) {
-                        eh_novo = true;
+                        arquivo_adicionado = true;
                     } else if (tempo_atual > meus_arquivos_estados[nome_arq].ultima_modificacao) {
                         foi_modificado = true;
                     } else {
                         verificar_rede = true;
                         versao_local = meus_arquivos_estados[nome_arq].versao;
                     }
-                } // Aqui o lock_leitura morre e a tabela fica livre!
+                } // Aqui o lock_leitura morre e a tabela fica livre
 
-                // ===================================================
-                // PASSO 2: AÇÃO NA REDE (Livre de Travas!)
-                // ===================================================
+                //ações que podem ser tomadas de acordo com as verificações realizadas acima
                 try {
-                    if (eh_novo) {
+                    if (arquivo_adicionado) {
                         std::cout << "\n[AUTO-SYNC] Novo arquivo detectado localmente: " << nome_arq << "\n";
                         int nova_v = tracker.call("registrar_peer", meu_usuario, nome_arq, privado, upload).as<int>();
                         
-                        // PASSO 3: ESCRITA RÁPIDA
+                        //escrita rápida
                         std::lock_guard<std::mutex> lock_escrita(acesso_tabela);
                         meus_arquivos_estados[nome_arq] = {nova_v, tempo_atual, privado};
                     } 
@@ -189,6 +180,7 @@ void sincronizador_automatico() {
                             std::cout << "[AUTO-SYNC] BLOQUEADO: Arquivo somente leitura ou privado!\n";
                             fazer_download(nome_arq, tracker);
                             
+                            //o arquivo coloca o tempo atual como o estado mais atual do arquivo mesmo sendo o mesmo
                             std::lock_guard<std::mutex> lock_escrita(acesso_tabela);
                             meus_arquivos_estados[nome_arq].ultima_modificacao = std::filesystem::last_write_time(entrada.path());
                         } else {
@@ -219,27 +211,31 @@ void sincronizador_automatico() {
     }
 }
 
-// ========================================================
-// 4. SERVIDOR DO CLIENTE & FUNÇÕES DO MENU
-// ========================================================
-
+// Função que recebe a requisição de outros nós e transferem os dados para o nó requirinte
 std::vector<char> transferir_arquivo(std::string nome_arquivo) {
+    //obtem o endereço até o arquivo requirido
     std::string caminho_completo = meu_diretorio + "/" + nome_arquivo;
     std::cout << "[PEER SERVIDOR] Pedido de download recebido para: " << nome_arquivo << "\n";
 
+    //ponteiro do arquivo
     std::ifstream arquivo(caminho_completo, std::ios::binary | std::ios::ate);
-    if (!arquivo.is_open()) return std::vector<char>(); 
+    if (!arquivo.is_open()) return std::vector<char>(); // se arquivo não abriu retorna sem conteúdo
 
+    //obtem o tamanho do conteúdo contido no arquivo
     std::streamsize tamanho = arquivo.tellg();
     arquivo.seekg(0, std::ios::beg);
     std::vector<char> buffer(tamanho);
 
+    // faz a leitura de dados
     if (arquivo.read(buffer.data(), tamanho)) {
         return buffer;
     }
+
+    //retorna os dados lidos
     return std::vector<char>();
 }
 
+// menu de cadastramento de arquivos, futuramente será um modificador de permissão de arquivo ou exlusão
 void cadastra_obj() {
     std::string arquivo;
     int permissao;
@@ -256,19 +252,17 @@ void cadastra_obj() {
         std::cout << "Qual e a permissao deste arquivo para novos usuarios?\n";
         std::cout << "\t 0 - Privado\n\t 1 - Somente leitura \n\t 2 - Leitura e escrita\n";
         std::cin >> permissao;
-    } while (permissao < 0 || permissao > 2); // CORREÇÃO: Usar || (OU) em vez de &&
+    } while (permissao < 0 || permissao > 2); 
     
     permissao += 3;
 
     std::cout << "%d" << permissao;
 
-    // Adicionado o parâmetro 'permissao'
     int estado = tracker.call("registrar_peer", meu_usuario, arquivo, permissao, upload).as<int>();
     
     if (estado == 1) {
         std::lock_guard<std::mutex> lock_escrita(acesso_tabela);
         std::cout << "Arquivo registrado com sucesso!\n";
-        meus_arquivos.push_back(arquivo);
         
         auto tempo_mod = std::filesystem::last_write_time(caminho_completo);
         meus_arquivos_estados[arquivo] = {1, tempo_mod, permissao};
@@ -290,10 +284,12 @@ void aquisicao_obj() {
     fazer_download(arquivo_requirido, tracker); 
 }
 
+// Menu de criação de arquivos e setador de permissões
 void criar_arquivo_txt() {
     std::string nome_arquivo, conteudo;
     int permissao;
 
+    // Obtenho todos os dados do arquivo para depois cria-lo, faço isso por condição de corrida ocorrida pelo sincronizador de arquivos, no qual antes dessa mudança as permições eram setadas por aquela função ao invés do usuário
     std::cout << "Nome do arquivo (ex: teste.txt): ";
     std::cin >> nome_arquivo;
 
@@ -302,10 +298,6 @@ void criar_arquivo_txt() {
     std::getline(std::cin, conteudo);
 
     std::string caminho_completo = meu_diretorio + "/" + nome_arquivo;
-    
-    
-   
-        
 
     do {
         std::cout << "Qual e a permissao desse arquivos para novos usuarios?\n";
@@ -325,13 +317,13 @@ void criar_arquivo_txt() {
                 arquivo << conteudo; 
                 arquivo.close();
                 std::cout << "[SISTEMA] Arquivo criado no disco!\n";
-                meus_arquivos.push_back(nome_arquivo);
+                
                 meus_arquivos_estados[nome_arquivo] = {1, std::filesystem::last_write_time(caminho_completo), permissao};
             }
         }
  }
 
-
+// Função que gera um arquivo base de testes, futuramente será removido
 void criar_arquivo_aleatorio(std::string nome_arquivo) {
     std::string caminho_completo = meu_diretorio + "/" + nome_arquivo;
     std::ofstream arquivo(caminho_completo);
@@ -342,6 +334,7 @@ void criar_arquivo_aleatorio(std::string nome_arquivo) {
     }
 }
 
+// função que realiza a leitura inicial do sistema de arquivos com base no usuário, realiza a leitura dos arquivos da pasta do usuário atual ou cria uma nova pasta se o usuário nunca acessou pelo computador. Modificações de instalação de arquivos nos quais pertencem ao usuário mas não estão no computador atual pode ser uma boa implementação
 void inicializar_sistema_de_arquivos() {
     meu_diretorio = "./pasta_" + meu_usuario;
 
@@ -369,10 +362,6 @@ void inicializar_sistema_de_arquivos() {
     }
 }
 
-// ========================================================
-// 5. MAIN (INICIALIZAÇÃO)
-// ========================================================
-
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         std::cerr << "Uso: ./cliente <sua_porta> <nome_usuario>\n";
@@ -390,12 +379,13 @@ int main(int argc, char *argv[]) {
 
     inicializar_sistema_de_arquivos();
 
+    // configuração de execução assincrona da única porta de recepção de dados do peer cliente, futuras implementações podem exigir porta fixa por conta do uso de mais de uma porta para a verificação de peers mais recomendados para fazer o download
     rpc::server srv(minha_porta);
     srv.bind("transferir_arquivo", &transferir_arquivo);
     srv.async_run(1); 
     std::cout << "[PEER] Escutando pedidos na porta " << minha_porta << "...\n";
 
-    std::thread thread_sync(sincronizador_automatico);
+    std::thread thread_sync(sincronizador_automatico); // cria uma thread para o sincronizador rodar em paralelo com o sistema do usuário sem atrapalhar a sua experiência
     thread_sync.detach(); 
 
     int op = 0; 
